341e6d2f9f4c476caf5094ef1f14ba6e51aeea83
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-26 17:43:37 +0800

    Intro to variable and data type

---
+++ Meeting 2/script.js
@@ -0,0 +1,7 @@
+var firstName = "Jeff";
+var lastName = "Chang";
+
+console.log("My name is " + firstName + lastName);
+
+const favoriteNumber = 8;
+console.log("My favorite numbers are " + favoriteNumber);
