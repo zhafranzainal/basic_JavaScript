67db7bde1aab8b6a0cc5d01562297a0ffc688e47
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-26 20:14:20 +0800

    Use assignment operator

---
+++ Meeting 3 - Operator and Expression/assignment-operator.js
@@ -0,0 +1,13 @@
+var a = 15; var b = 23;
+var c = 228; var d = 500;
+var e = 100;
+
+a = 17; b = 20;
+c = 3; d = 1000;
+e = 4;
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
+console.log(`c = ${c}`);
+console.log(`d = ${d}`);
+console.log(`e = ${e}`);
---
+++ Meeting 3 - Operator and Expression/displayData.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "Cobee",
+    age: 15,
+    goal: "Programmer"
+};
+
+console.log(`My name is ${person.name}, I am ${person.age} years old, and I want to be a ${person.goal} in the future.`);
