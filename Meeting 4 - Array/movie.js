92c75c6c483333fe9af693f207b77ff54809f882
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-26 20:36:46 +0800

    Create movie.js

---
+++ Meeting 4 - Array/movie.js
@@ -0,0 +1,14 @@
+var movies = [
+    "Interstellar",
+    "Oppenheimer",
+    "Spider-Man",
+    "Avatar",
+    "The Avengers"
+];
+
+// Access individual movies by their index (0 to 4)
+console.log("1st Movie: " + movies[0]);
+console.log("2nd Movie: " + movies[1]);
+console.log("3rd Movie: " + movies[2]);
+console.log("4th Movie: " + movies[3]);
+console.log("5th Movie: " + movies[4]);
