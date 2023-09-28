b369585de2fbcece65906ee8b5947a2f88483e87
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-28 12:09:07 +0800

    Create stringNumberPrinting.js

---
+++ Meeting 7 - For Loop/stringNumberPrinting.js
@@ -0,0 +1,18 @@
+// Printing string 20 times
+for (i = 0; i < 20; i++) {
+    console.log("I'm Steve");
+}
+
+console.log("");
+
+// Printing numbers from 1 to 20
+for (i = 0; i < 20; i++) {
+    console.log(i + 1);
+}
+
+console.log("");
+
+// Printing numbers from 5 to 100 in multiples of 5
+for (i = 5; i <= 100; i += 5) {
+    console.log(i);
+}
