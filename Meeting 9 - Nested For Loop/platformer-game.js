e920741e6c5cbd125804f767be1b5e460e57674c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-01 13:57:45 +0800

    Update platformer-game.js

--- Meeting 9 - Nested For Loop/platformer-game.js
+++ Meeting 9 - Nested For Loop/platformer-game.js
@@ -2,8 +2,9 @@ for (let i = 1; i <= 3; i++) {
 
     console.log(`Platform created ${i}`);
 
-    for (let i = 1; i <= 2; i++) {
-        console.log(`-- Carrot created ${i}`);
+    for (let j = 1; j <= 2; j++) {
+        console.log(`-- Carrot created ${j}`);
     }
 
 }
+
