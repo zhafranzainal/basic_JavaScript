b8b87ea9d46cf47e24e0783ca5ea87d6eb2ab2a4
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-03 19:43:32 +0800

    Create find-number.js

---
+++ Meeting 14 - For Each/find-number.js
@@ -0,0 +1,12 @@
+var numbers = [111, 11, 1, 111, 1111, 11, 11, 111, 1, 111];
+
+numbers.forEach(function (number, index) {
+
+    if (number === 1111) {
+        console.log(`${number} located at index ${index}`);
+        return; // This won't exit the forEach loop, but it will skip next iteration
+    }
+
+    console.log(`Loop ${index}`);
+
+});
