e53c6b1f98c1faf846d19a4abb32b6e1baa07793
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-03 19:56:48 +0800

    Create greeting.js

---
+++ Meeting 14 - For Each/greeting.js
@@ -0,0 +1,11 @@
+var names = ["Mike", "Noah", "Olive", "Peter", "Ryan"];
+
+names.forEach(function (name) {
+    console.log(`Hi ${name}`);
+});
+
+console.log("");
+
+names.forEach(name => {
+    console.log(`Hi ${name}`);
+});
